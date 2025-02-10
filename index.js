6ac292362bac9b1fe6886ac1b4fef0641129af76
RetardedFucker500 <prodleland67@gmail.com>
2025-02-10 09:03:04 -0500

    add html files

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'menu.html'));
+});
+
+// Start the server
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
